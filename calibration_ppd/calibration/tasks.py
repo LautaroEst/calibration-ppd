86969b6a7dbc6f035818eba9b13e19a804222ca9
Gonzalo <gonzalorpg@gmail.com>
2022-11-22 10:28:35 -0300

    más arreglos

--- calibration_ppd/calibration/tasks.py
+++ calibration_ppd/calibration/tasks.py
@@ -10,13 +10,13 @@ from scipy.special import expit
 class DiscriminativeModelCalibration(Task):
 
     def __init__(self,model):
-
+        logsigmoid = torch.nn.LogSigmoid()
         modelOutputs = self.load("00_train_system/"+model)        
         self.model = model
         self.priors = None #podemos probar cambiando esto
         self.targetsTraining = torch.tensor(modelOutputs["train"]["labels"],dtype=torch.int64)
-        self.logPosteriorsTraining = torch.tensor(expit(modelOutputs["train"]["logits"]),dtype=torch.float32)
-        self.logPosteriorsValidation = torch.tensor(expit(modelOutputs["validation"]["logits"]),dtype=torch.float32)
+        self.logPosteriorsTraining = logsigmoid(torch.tensor(modelOutputs["train"]["logits"],dtype=torch.float32))
+        self.logPosteriorsValidation = logsigmoid(torch.tensor(modelOutputs["validation"]["logits"],dtype=torch.float32))
 
     def run(self):
         calibratedLogPosteriors, parameters = calibrate(self.logPosteriorsTraining, self.targetsTraining, self.logPosteriorsValidation, AffineCalLogLoss, bias=True, priors=self.priors, quiet=True)
