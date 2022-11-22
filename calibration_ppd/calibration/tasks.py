949958e2b36a6804fb934cef503bcbcc5d771a0b
Gonzalo <gonzalorpg@gmail.com>
2022-11-22 10:22:42 -0300

    updates y arreglos

--- calibration_ppd/calibration/tasks.py
+++ calibration_ppd/calibration/tasks.py
@@ -3,7 +3,7 @@ from ..core import Task
 import numpy as np
 import torch
 import pickle
-
+from scipy.special import expit
 
 
 
@@ -15,8 +15,8 @@ class DiscriminativeModelCalibration(Task):
         self.model = model
         self.priors = None #podemos probar cambiando esto
         self.targetsTraining = torch.tensor(modelOutputs["train"]["labels"],dtype=torch.int64)
-        self.logPosteriorsTraining = torch.tensor(-np.logaddexp(0,modelOutputs["train"]["logits"]),dtype=torch.float32)
-        self.logPosteriorsValidation = torch.tensor(-np.logaddexp(0,modelOutputs["validation"]["logits"]),dtype=torch.float32)
+        self.logPosteriorsTraining = torch.tensor(expit(modelOutputs["train"]["logits"]),dtype=torch.float32)
+        self.logPosteriorsValidation = torch.tensor(expit(modelOutputs["validation"]["logits"]),dtype=torch.float32)
 
     def run(self):
         calibratedLogPosteriors, parameters = calibrate(self.logPosteriorsTraining, self.targetsTraining, self.logPosteriorsValidation, AffineCalLogLoss, bias=True, priors=self.priors, quiet=True)
