4eb977f87e95f4b1f8cb9eb2ee99db42d832a921
Gonzalo <gonzalorpg@gmail.com>
2022-11-22 10:58:30 -0300

    era la logit de la posterior

--- calibration_ppd/calibration/tasks.py
+++ calibration_ppd/calibration/tasks.py
@@ -15,16 +15,16 @@ class DiscriminativeModelCalibration(Task):
         self.model = model
         self.priors = None #podemos probar cambiando esto
         self.targetsTraining = torch.tensor(modelOutputs["train"]["labels"],dtype=torch.int64)
-        self.logPosteriorsTraining = logsigmoid(torch.tensor(modelOutputs["train"]["logits"],dtype=torch.float32))
-        self.logPosteriorsValidation = logsigmoid(torch.tensor(modelOutputs["validation"]["logits"],dtype=torch.float32))
+        self.logPosteriorsTraining = torch.tensor(modelOutputs["train"]["logits"],dtype=torch.float32)
+        self.logPosteriorsValidation = torch.tensor(modelOutputs["validation"]["logits"],dtype=torch.float32)
 
     def run(self):
         calibratedLogPosteriors, parameters = calibrate(self.logPosteriorsTraining, self.targetsTraining, self.logPosteriorsValidation, AffineCalLogLoss, bias=True, priors=self.priors, quiet=True)
         #cambiando el parámetro bias habilita o deshabilita el temp_scaling
-        return {"logprobs":calibratedLogPosteriors.detach().numpy()}
+        return {"logits":calibratedLogPosteriors.detach().numpy()}
 
     def save(self,output,output_dir):
-        np.savez_compressed(output_dir+"/results.npz",logprobs=output["logprobs"])
+        np.savez_compressed(output_dir+"/results.npz",logits=output["logits"])
 
     def load(self,output_dir):
         with open(output_dir+"_results.pkl", 'rb') as f:
