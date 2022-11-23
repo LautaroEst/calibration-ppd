61b09889b79ca5c56a7f1f482a7ed0f375c8dad3
LautaroEst <lautaro.est@gmail.com>
2022-11-22 21:13:21 -0300

    analisis de resultados. No anda la calibración

---
+++ experiments/01_calibrate_system/configs/cbow_bias=True_priors=None.py
@@ -0,0 +1,35 @@
+
+
+n_jobs = 1
+
+
+def make_pipeline(
+    model_name,
+    bias,
+    priors,
+    calibration_loss
+):
+    return [
+        ("Loading Predictions", {
+            "task": "LoadModelPredictions",
+            "input": None,
+            "path": f"01_calibrate_system/{model_name}_results.pkl",
+            "output": "results"
+        }),
+        ("Calibration", {
+            "task": "DiscriminativeModelCalibration",
+            "input": {"model_outputs": "results"},
+            "output": "calibratedLogPosteriors",
+            "bias": bias, 
+            "priors": priors,
+            "calibration_loss": calibration_loss
+        })
+    ]
+
+pipeline = make_pipeline(
+    model_name = "cbow",
+    bias = True, # cambiando el parámetro bias habilita o deshabilita el temp_scaling
+    priors = [0.5], # podemos probar cambiando esto
+    calibration_loss = "affine_ece"
+)
+runs = {0: pipeline}
